263e358cf12f9c74ddcd691518139dcc3c3103dc
Hans Tovetjärn <hans.tovetjarn@gmail.com>
2012-03-12 03:12:26 +0100

    Tweaks

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,13 +3,16 @@ import System.Exit
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Layout.ToggleLayouts
 import XMonad.Layout.Spacing
+import XMonad.Layout.Gaps
+import XMonad.Layout.Renamed
+import XMonad.Layout.LimitWindows
+import XMonad.Layout.Dishes
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
  
 myTerminal				=	"urxvtc"
-myBorderWidth			=	2
+myBorderWidth			=	0
 myModMask				=	mod4Mask
 myWorkspaces			=	["ZSH","VIM","WEB","IRC","GFX","MSC"]
 myNormalBorderColor		=	"#161616"
@@ -21,32 +24,24 @@ myFocusedBorderColor	=	"#ee3216"
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 	[
-		((modm, xK_slash), spawn $ XMonad.terminal conf), -- Init a terminal
-		((modm, xK_space), spawn "dmenu_run -fn 'Terminus:bold:size=16' -nb '#000' -nf '#868686' -sb '#868686' -sf '#fff' -h 20"), -- Init dmenu_run
+		((modm, xK_space), spawn "dmenu_run -fn 'xft:Droid Sans Bold:size=14' -nb '#000000' -nf '#868686' -sb '#868686' -sf '#ffffff'"), -- Launcher
+		((modm, xK_Return), spawn $ XMonad.terminal conf), -- Terminal
+		((modm, xK_apostrophe), spawn "qvim"), -- QVim
+		((modm, xK_slash), spawn "slock"), -- Lock screen
+		((modm, xK_backslash), spawn "xmonad --recompile; xmonad --restart"),
+		((modm, xK_grave), io (exitWith ExitSuccess)),
+		
 		((modm, xK_Tab), windows W.focusDown), -- Move focus to the next window
 		((modm .|. shiftMask, xK_Tab), sendMessage NextLayout), -- Rotate through available layouts
-		((modm, xK_Return), sendMessage ToggleLayout), -- Toggle fullscreen mode
-		((modm, xK_n), prevWS), -- 
-		((modm, xK_o), nextWS), -- 
-		((modm .|. shiftMask, xK_n), shiftToPrev >> prevWS), -- 
-		((modm .|. shiftMask, xK_o), shiftToNext >> nextWS), -- 
-		--((modm .|. shiftMask,	xK_c), kill), -- close focused window
-		--((modm .|. shiftMask,	xK_space), setLayout $ XMonad.layoutHook conf), -- Reset the layouts on current workspace to default
-		--((modm, xK_n), refresh), -- Resize viewed windows to the correct size
-		--((modm, xK_j), windows W.focusDown), -- Move focus to the next window
-		--((modm, xK_k), windows W.focusUp), -- Move focus to the previous window
-		--((modm, xK_m), windows W.focusMaster), -- Move focus to the master window
-		--((modm, xK_Return), windows W.swapMaster), -- Swap the focused window and the master window
-		--((modm .|. shiftMask, xK_j), windows W.swapDown), -- Swap the focused window with the next window
-		--((modm .|. shiftMask,	xK_k), windows W.swapUp), -- Swap the focused window with the previous window
-		--((modm, xK_h), sendMessage Shrink), -- Shrink the master area
-		--((modm, xK_l), sendMessage Expand), -- Expand the master area
-		--((modm, xK_t), withFocused $ windows . W.sink), -- Push window back into tiling
-		--((modm, xK_comma), sendMessage (IncMasterN 1)), -- Increment number of windows in the master area
-		--((modm, xK_period), sendMessage (IncMasterN (-1))), -- Deincrement number of windows in the master area
-		((modm, xK_grave), io (exitWith ExitSuccess)), -- Quit xmonad
-		((modm, xK_comma), spawn "slock"), -- Lock screen
-		((modm, xK_period), spawn "xmonad --recompile; xmonad --restart") -- Restart xmonad
+		((modm, xK_n), prevWS),
+		((modm, xK_o), nextWS),
+		((modm .|. shiftMask, xK_n), shiftToPrev >> prevWS),
+		((modm .|. shiftMask, xK_o), shiftToNext >> nextWS),
+		((modm .|. shiftMask, xK_k), kill), -- close focused window
+		((modm, xK_m), windows W.swapMaster), -- Make focused window master window
+		((modm, xK_comma), sendMessage Shrink), -- Shrink the master area
+		((modm, xK_period), sendMessage Expand), -- Expand the master area
+		((modm, xK_t), withFocused $ windows . W.sink) -- Push window back into tiling
 	]
     ++
  
@@ -73,17 +68,26 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- Layouts
 --------------------------------------------------------------------------------------
 
+myFocus = gaps [(L,256), (R,256)] $ spacing 4 $ limitWindows 2 $ Dishes nmaster ratio
+	where
+		-- The default number of windows in the master pane
+		nmaster = 1
+		-- Default proportion of screen occupied by other panes
+		ratio = 1/5
+
+myFull = Full
+
+myDiscs = spacing 4 $ limitWindows 5 $ Dishes nmaster ratio
+	where
+		-- The default number of windows in the master pane
+		nmaster = 1
+		-- Default proportion of screen occupied by other panes
+		ratio = 1/5
+
 myLayout =
 	avoidStruts $
-	spacing 4 $
-	toggleLayouts Full $
-	tiled ||| Mirror tiled
-	where
-		tiled = Tall nmaster delta ratio -- default tiling algorithm parts screen into two panes
-		nmaster = 1 -- The default number of windows in the master pane
-		ratio = 1/2 -- Default proportion of screen occupied by master pane
-		delta = 2/100 -- Percent of screen to increment by when resizing panes
- 
+	renamed [Replace "Focus"] myFocus ||| renamed [Replace "Full"] myFull ||| renamed [Replace "Discs"] myDiscs
+
 --------------------------------------------------------------------------------------
 -- Window rules
 --------------------------------------------------------------------------------------
@@ -108,7 +112,7 @@ myPP = xmobarPP
 			ppUrgent = xmobarColor "#ff0000" "",
 			ppTitle = xmobarColor "#ffffff" "",
 			ppLayout = xmobarColor "#aaaaaa" "",
-			ppOrder = \(ws:_:t:_) -> [ws,t], -- Don't display current layout
+			--ppOrder = \(ws:_:t:_) -> [ws,t], -- Don't display current layout
 			ppSep = " » ",
 			ppWsSep = " "
 		}
