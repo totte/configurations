af9a79baf69ba56c3fae86632ceec98c68c60046
totte <totte@tott.es>
2012-09-29 18:23:27 +0000

    Correct setting for usage of Xft font in dmenu and height

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -24,7 +24,7 @@ myFocusedBorderColor	        =   "#080808"
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 	[
-		((modm, xK_space), spawn "dmenu_run -fn 'xft:Chicago:size=16' -nb '#000000' -nf '#868686' -sb '#868686' -sf '#ffffff'"), -- Launcher
+		((modm, xK_space), spawn "dmenu_run -fn 'Chicago-14:style=Bold' -nb '#000000' -nf '#868686' -sb '#868686' -sf '#ffffff' -h 32"), -- Launcher
 		((modm, xK_Return), spawn $ XMonad.terminal conf), -- Terminal
 		((modm, xK_apostrophe), spawn "qvim"), -- QVim
 		((modm, xK_slash), spawn "slock"), -- Lock screen
