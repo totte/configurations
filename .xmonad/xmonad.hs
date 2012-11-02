75d951522703d2e13af43cfe9d2cc93135d67bbe
totte <totte@tott.es>
2012-11-03 00:05:02 +0100

    Rely on XMonad prompt for launching programs and finding windows

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,5 @@
 import XMonad
 import XMonad.Actions.CycleWS
-import XMonad.Actions.GridSelect
 import XMonad.Actions.UpdatePointer
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -10,98 +9,63 @@ import XMonad.Layout.Renamed
 import XMonad.Layout.SimplestFloat
 import XMonad.Prompt
 import XMonad.Prompt.Shell
+import XMonad.Prompt.Window
 import System.Exit
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 
-myTerminal              =   "konsole"
-myBorderWidth           =   2
-myFocusedBorderColor    =   "#2c72c7"
-myNormalBorderColor     =   "#080808"
-myWorkspaces            =   ["ZSH","VIM","QTC","WEB","IRC","PIM","MPD","ETC"]
-myModMask               =   mod4Mask
+myFont              = "xft:Ubuntu:style=Bold:size=14"
+myWhiteColor        = "#ffffff"
+myLightGreyColor    = "#aaaaaa"
+myGreyColor         = "#646464"
+myDarkGreyColor     = "#323232"
+myBlackColor        = "#000000"
+myBlueColor         = "#2c72c7"
+myRedColor          = "#e20800"
+myBorderWidth       = 2
+myTerminal          = "konsole"
+myBar               = "xmobar"
+myWorkspaces        = ["ZSH","VIM","QTC","WEB","IRC","PIM","MPD","ETC"]
+myModMask           = mod4Mask
+myAltMask           = mod1Mask
+myLogHook = updatePointer (Relative 0.98 0.94)
 
 -- Keybindings
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [
-        -- Switch to next layout
         ((modm, xK_Tab), sendMessage NextLayout),
-        
-        -- Lock screen
         ((modm, xK_Escape), spawn "slock"),
-        
-        -- Exit session
         ((modm .|. shiftMask, xK_Escape), io (exitWith ExitSuccess)),
-        
-        -- Recompile and restart XMonad
         ((modm, xK_r), spawn "xmonad --recompile; xmonad --restart"),
-        
-        -- Run terminal
         ((modm, xK_Return), spawn $ XMonad.terminal conf),
-        
-        -- Switch to previous workspace
         ((modm, xK_n), prevWS),
-        
-        -- Move focused window to previous workspace
         ((modm .|. shiftMask, xK_n), shiftToPrev >> prevWS),
-        
-        -- Move focus to the previous window
         ((modm, xK_e), windows W.focusDown),
-        
-        -- Move focus to the next window
         ((modm, xK_i), windows W.focusUp),
-        
-        -- Switch to next workspace
         ((modm, xK_o), nextWS),
-        
-        -- Move focused window to next workspace
         ((modm .|. shiftMask, xK_o), shiftToNext >> nextWS),
-        
-        -- Kill focused window
         ((modm, xK_k), kill),
-        
-        -- Make focused window master window
         ((modm, xK_m), windows W.swapMaster),
-        
-        -- Shrink the master area
         ((modm, xK_comma), sendMessage Shrink),
-        
-        -- Expand the master area
         ((modm, xK_period), sendMessage Expand),
-        
-        -- Lower master volume
         ((0, 0x1008FF11), spawn "amixer set Master 2-"),
-        
-        -- Toggle master output
         ((0, 0x1008FF12), spawn "amixer set Master toggle"),
-        
-        -- Raise master volume
         ((0, 0x1008FF13), spawn "amixer set Master 2+"),
-
-        -- Screenshot
-        ((modm, xK_a), spawn "scrot '%Y-%m-%d_$wx$h.png' -t 10 -e 'mv $f ~/pictures/screenshots/' -e 'mv $m ~/pictures/screenshots/'"),
-
-        -- Grid: select and switch to window
-        ((modm, xK_t), goToSelected defaultGSConfig),
-
-        -- Prompt: run program
-        ((modm, xK_space), shellPrompt myXPConfig)
+        ((modm, xK_s), spawn "scrot '%Y-%m-%d_$wx$h.png' -t 10 -e 'mv $f ~/pictures/screenshots/' -e 'mv $m ~/pictures/screenshots/'"),
+        ((modm, xK_space), shellPrompt myXPConfig),
+        ((modm .|. myAltMask, xK_space), windowPromptGoto myXPConfig)
     ]
 
+-- Mouse keybindings
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     [
-        -- Float and move window
         ((modm, button1), (\w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster)),
-        
-        -- Float and resize window
         ((modm, button2), (\w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster)),
-        
-        -- Push back into tiling
         ((modm, button3), windows . W.sink)
     ]
 
 -- Layouts
-myFull = Full
+myFull = noBorders Full
 myWide = Mirror $ Tall nmaster delta ratio
     where
         nmaster = 1
@@ -110,7 +74,7 @@ myWide = Mirror $ Tall nmaster delta ratio
 myFloat = simplestFloat
 myLayout =
     avoidStruts $
-        noBorders (renamed [Replace "Full"] myFull) ||| renamed [Replace "Wide"] myWide ||| renamed [Replace "Float"] myFloat
+        renamed [Replace "Full"] myFull ||| renamed [Replace "Wide"] myWide ||| renamed [Replace "Float"] myFloat
 
 -- Window rules
 myManageHook = composeAll
@@ -126,20 +90,16 @@ myManageHook = composeAll
         className =? "xmessage" --> doCenterFloat
     ]
 
--- Other rules
-myLogHook = updatePointer (Relative 0.98 0.94)
-
 -- Panel
-myBar = "xmobar"
 myPP = xmobarPP
     {
-        ppCurrent = wrap "<fc=#ffffff,#2c72c7> " " </fc>",
-        ppVisible = xmobarColor "#ff0000" "",
-        ppHidden = xmobarColor "#646464" "",
-        ppHiddenNoWindows = xmobarColor "#646464" "",
-        ppUrgent = xmobarColor "#ff0000" "",
-        ppTitle = xmobarColor "#ffffff" "" . shorten 86,
-        ppLayout = xmobarColor "#aaaaaa" "",
+        ppCurrent = xmobarColor myBlueColor "",
+        ppVisible = xmobarColor myBlueColor "",
+        ppHidden = xmobarColor myGreyColor "",
+        ppHiddenNoWindows = xmobarColor myDarkGreyColor "",
+        ppUrgent = xmobarColor myRedColor "",
+        ppTitle = xmobarColor myWhiteColor "" . shorten 86,
+        ppLayout = xmobarColor myLightGreyColor "",
         ppSep = " » ",
         ppWsSep = " "
     }
@@ -147,26 +107,26 @@ myPP = xmobarPP
 -- Prompt
 myXPConfig = defaultXPConfig
     {
-        font = "xft:Ubuntu:style=Bold:size=14",
-        bgColor = "#000000",
-        fgColor = "#ffffff",
-        bgHLight = "#000000",
-        fgHLight = "#2c72c7",
-        borderColor = "#323232",
+        font = myFont,
+        bgColor = myBlackColor,
+        fgColor = myWhiteColor,
+        bgHLight = myBlackColor,
+        fgHLight = myBlueColor,
+        borderColor = myBlackColor,
         promptBorderWidth = 0,
         position = Top,
         height = 32
     }
 
--- ???
+-- Put together all configurations
 myConfig = defaultConfig
     {
         terminal = myTerminal,
         borderWidth = myBorderWidth,
         modMask = myModMask,
         workspaces = myWorkspaces,
-        normalBorderColor = myNormalBorderColor,
-        focusedBorderColor = myFocusedBorderColor,
+        normalBorderColor = myBlackColor,
+        focusedBorderColor = myBlueColor,
         keys = myKeys,
         mouseBindings = myMouseBindings,
         layoutHook = myLayout,
